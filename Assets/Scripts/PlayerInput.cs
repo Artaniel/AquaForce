e0fac4422a156cce2800d37de6a5903188e73142
Artaniel <59449523+Artaniel@users.noreply.github.com>
2026-04-17 23:40:29 +0300

    inpur

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,15 +1,34 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInput: MonoBehaviour
 {
     private Game _game;
+
+    public Vector2 cursorWorldPosition;
+    public bool cursorIsPressed;
+    public bool cursorWasPressedThisFrame;
+    public bool cursorWasReleasedThisFrame;
+    public Action OnCursorPress;
+    public Action OnCursorRelease;
 
     public void Init(Game game) {
         _game = game;
     }
 
     private void Update() {
-        
+        DesktopUpdate();
+    }
+
+    private void DesktopUpdate() {
+        cursorIsPressed = Mouse.current.leftButton.isPressed;
+        cursorWasPressedThisFrame = Mouse.current.leftButton.wasPressedThisFrame;
+        cursorWasReleasedThisFrame = Mouse.current.leftButton.wasReleasedThisFrame;
+        if (cursorWasPressedThisFrame) OnCursorPress?.Invoke();
+        if (cursorWasReleasedThisFrame) OnCursorRelease?.Invoke();
+
+        cursorWorldPosition = _game.mainCamera.ScreenToWorldPoint(Mouse.current.position.value);
     }
 
 }
