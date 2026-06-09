9df951c1a2ce5900b85b882bf0134b55fd8ff86b
Artaniel <59449523+Artaniel@users.noreply.github.com>
2026-06-09 16:09:34 +0300

    sparcles particles failed

--- Assets/Scripts/Wave/WaveParticles.cs
+++ Assets/Scripts/Wave/WaveParticles.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 public class WaveParticles : MonoBehaviour
 {
     private WaterWave _waterWave;
-    public ParticleSystem particles;
-    private ParticleSystem.ShapeModule _shape;
-    private ParticleSystem.EmissionModule _emission;
+    public ParticleSystem foamParticles;
+    private ParticleSystem.ShapeModule _foamShape;
+    private ParticleSystem.EmissionModule _foamEmission;
     public float emissionRateMultiplier = 10f;
     public float minimalRadius = 0.1f;
+    public ParticleSystem sparcleParticles;
+    private ParticleSystem.ShapeModule _sparcleShape;
+    private ParticleSystem.EmissionModule _sparcleEmission;
+
+
     
     public void Init(WaterWave waterWave) {
         _waterWave = waterWave;
-        _shape = particles.shape;
-        _emission = particles.emission;
+        _foamShape = foamParticles.shape;
+        _foamEmission = foamParticles.emission;
+        _sparcleShape = sparcleParticles.shape;
+        _sparcleEmission = sparcleParticles.emission;
     }
     
     public void RefreshParticleCloudRadius(float radius) {
-        _shape.radius = radius;
-        _emission.rateOverDistance = radius * emissionRateMultiplier - minimalRadius;
+        _foamShape.radius = radius;
+        _foamEmission.rateOverDistance = radius * emissionRateMultiplier - minimalRadius;
+        _sparcleShape.radius = 0.2f * radius;
+        _sparcleEmission.rateOverTime = 5f * radius;
     }
 }
