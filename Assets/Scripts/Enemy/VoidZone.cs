86dbd7035668ecfe4af9c0d9c44eb09d4122f922
Artaniel <59449523+Artaniel@users.noreply.github.com>
2026-06-28 22:18:00 +0300

    sound

--- Assets/Scripts/Enemy/VoidZone.cs
+++ Assets/Scripts/Enemy/VoidZone.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class VoidZone : MonoBehaviour
 {
+    private Game _game;
+
+    public void Init(Game game){
+        _game = game;
+    }
+
     void OnTriggerEnter2D(Collider2D collision) {
         Enemy enemy = collision.GetComponent<Enemy>();
         if (!enemy) return;
         enemy.health.Kill();
+        _game.sound.OnVoidZoneDeath();
     }
 }
