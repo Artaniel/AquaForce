8b0428061bf654238a355a6f031f0057a3e0e0e6
Artaniel <59449523+Artaniel@users.noreply.github.com>
2026-07-06 22:58:03 +0300

    goblin sounds

--- Assets/Scripts/Enemy/EnemySound.cs
+++ Assets/Scripts/Enemy/EnemySound.cs
@@ -10,20 +10,30 @@ public class EnemySound : MonoBehaviour
     public AudioSource struggleAudioSource;
     public AudioSource deathAudioSource;    
 
+    public AudioClip[] hitClips;
+    public AudioClip[] struggleClips;
+    public AudioClip[] deathClips;
+
     public void Init(Game game, Enemy enemy) {
         _game = game;
         _enemy = enemy;        
     } 
 
     public void Hit() {
+        if (hitClips.Length == 0) return;
+        hitAudioSource.clip = hitClips[Random.Range(0, hitClips.Length)];
         hitAudioSource.Play();
     }
 
     public void Struggle() {
+        if (struggleClips.Length == 0) return;
+        struggleAudioSource.clip = struggleClips[Random.Range(0, struggleClips.Length)];
         struggleAudioSource.Play();
     }
 
     public void Death() {
+        if (deathClips.Length == 0) return;
+        deathAudioSource.clip = deathClips[Random.Range(0, deathClips.Length)];
         deathAudioSource.Play();
     }
 
