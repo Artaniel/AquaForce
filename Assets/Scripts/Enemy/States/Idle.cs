ab5363de281c63d8aac0afd8c01d27001a4ce8f7
Artaniel <59449523+Artaniel@users.noreply.github.com>
2026-09-02 20:50:45 +0300

    wandering in idle state

--- Assets/Scripts/Enemy/States/Idle.cs
+++ Assets/Scripts/Enemy/States/Idle.cs
@@ -3,9 +3,16 @@ public class Idle : AiState
 {
     private float scanInterval = 0.5f;
     private float lastScanTime = 0f;
+    private float wanderStartTime;  
+    private bool isWandering = false;
+    private Vector2 wanderDir;
+    private float wanderCooldown = 1f;
+    private float wanderDuration = 1f;
+    private float wanderSpeed = 1f;
 
     public override void StartState() {
         lastScanTime = Time.time;
+        wanderStartTime = Time.time;
     }
 
     public override void StopState() {
@@ -13,6 +20,24 @@ public class Idle : AiState
     }
 
     public override void UpdateState(float deltaTime) {
+        float now = Time.time;
+
+        if (!isWandering) {
+            if (now >= wanderStartTime + wanderCooldown) {
+                isWandering = true;
+                wanderStartTime = now;
+                wanderDir = Random.insideUnitCircle.normalized;
+            }
+        } else {
+            if (now >= wanderStartTime + wanderDuration) {
+                isWandering = false;
+                wanderStartTime = now;
+            } else {
+                _ai.MoveInDirrection(wanderDir * wanderSpeed);
+                _owner.view.isLookingLeft = wanderDir.x < 0;
+            }
+        }
+
         if (Time.time - lastScanTime < scanInterval) return;
         lastScanTime = Time.time;
         
