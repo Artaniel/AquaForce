165ce196688da1dd8ca1e42e4e883f484bf8f0b2
Artaniel <59449523+Artaniel@users.noreply.github.com>
2026-09-14 23:03:02 +0300

    particles cleanup

--- Assets/Scripts/Wave/WaveParticles.cs
+++ Assets/Scripts/Wave/WaveParticles.cs
@@ -9,24 +9,15 @@ public class WaveParticles : MonoBehaviour
     private ParticleSystem.EmissionModule _foamEmission;
     public float emissionRateMultiplier = 10f;
     public float minimalRadius = 0.1f;
-    public ParticleSystem sparcleParticles;
-    private ParticleSystem.ShapeModule _sparcleShape;
-    private ParticleSystem.EmissionModule _sparcleEmission;
-
-
     
     public void Init(WaterWave waterWave) {
         _waterWave = waterWave;
         _foamShape = foamParticles.shape;
         _foamEmission = foamParticles.emission;
-        _sparcleShape = sparcleParticles.shape;
-        _sparcleEmission = sparcleParticles.emission;
     }
     
     public void RefreshParticleCloudRadius(float radius) {
         _foamShape.radius = radius;
         _foamEmission.rateOverDistance = radius * emissionRateMultiplier - minimalRadius;
-        _sparcleShape.radius = 0.2f * radius;
-        _sparcleEmission.rateOverTime = 5f * radius;
     }
 }
